c5ed608ddd4084d9409e8fc64f8aeb7d6d176e0e
Wendy Wang <wendywangg928@gmail.com>
2023-10-22 14:23:09 -0400

    added three routes so far and button functionality all works

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
+from petfax import create_app
+app = create_app()
 
 
-
-@app.route('/')
-def index():
-    return 'Hello this is PetFax'
-
-
-@app.route('/pets')
-def pets():
-    return 'Here are some of our pets up for adoption!'
